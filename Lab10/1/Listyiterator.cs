583a2c7f550be7312827d56c5b5d0b58f8a53145
bigzerooo <ruslanskibinskii@gmail.com>
2019-11-10 20:55:56 +0200

    Lab10 begin
    Lab10 begin

---
+++ Lab10/1/Listyiterator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+class ListyIterator<T> : IEnumerator<T>
+{
+    List<T> List; //Список итератора
+    int index; //индекс итератора
+    public ListyIterator()//Создание листа в конструкторе
+    {
+        List = new List<T>();
+        index = 0;
+    }
+    public ListyIterator(List<T> list)//Передача листа извне
+    {
+        List = list;
+        index = 0;
+    }
+    public T Current // Поточный элемент (возвращает T)
+    {
+        get
+        {
+            if (List.Count == 0 || index >= List.Count)
+                throw new InvalidOperationException("Invalid Operation!");
+            else
+                return List[index];
+        }
+    }
+    object IEnumerator.Current// Поточный элемент (возвращает object)
+    {
+        get
+        {
+            return Current; 
+        }
+    }
+    public void Print() //Вывод поточного элемента
+    {
+        Console.WriteLine(Current); 
+    }
+    public bool MoveNext()//Инкремент индекса
+    {
+        if (index < List.Count - 1)
+        {
+            index++;
+            return true;
+        }
+        else
+            return false;   
+    }
+    public bool Move()//По сути тот же MoveNext, но в заданиях другое название
+    {
+        return MoveNext();
+    }
+    public bool HasNext()//Есть ли следующий элемент?
+    {
+        if (index < List.Count - 1)
+        {            
+            return true;
+        }
+        else
+            return false;
+    }
+    public void Reset()//Обнуление индекса
+    {
+        index = 0;
+    }
+    public void Dispose()//Деструктор неуправляемих обьектов (не реализован)
+    {
+        
+    }
+}
---
+++ Lab10/1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] startTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);//Всегда Create [элементы]
+            ListyIterator<string> iterator;
+            if (startTokens.Length > 1)
+            {
+                List<string> list = new List<string>();
+                for (int i = 1; i < startTokens.Length; i++)                    
+                    list.Add(startTokens[i]);
+                iterator = new ListyIterator<string>(list);
+            }
+            else
+            {
+                iterator = new ListyIterator<string>();
+            }
+            for(int i=0;i<100;i++)//Сто команд
+            {
+                try
+                {                
+                    string[] tokens = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                    string command = tokens[0];
+                    command = command.ToLower();
+                    switch (command)
+                    {
+                        case "move":
+                            Console.WriteLine(iterator.Move());
+                            break;
+                        case "print":
+                            iterator.Print();
+                            break;
+                        case "hasnext":
+                            Console.WriteLine(iterator.HasNext());
+                            break;
+                        case "end":
+                            i = 100;//выход
+                            break;
+                        default:
+                            continue;
+                    }
+                }
+                catch(Exception x)
+                {
+                    Console.WriteLine(x.Message);
+                }
+            }
+        }
+    }
+}
